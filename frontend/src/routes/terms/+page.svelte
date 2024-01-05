02dd37ae804cdedc5d2413f33824c8a285ccfec3
Boofdev <hexahigh0@gmail.com>
2024-01-05 13:46:05 +0100

    update terms

--- frontend/src/routes/terms/+page.svelte
+++ frontend/src/routes/terms/+page.svelte
@@ -39,106 +39,47 @@
 	    <li>You agree that you must evaluate, and bear all risks associated with, the use of any Content, including any reliance on the accuracy, completeness, or usefulness of such Content.</li>
 	    <li>You acknowledge, consent and agree that YAPC may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to: (a) comply with legal process; (b) enforce the TOS; (c) respond to claims that any Content violates the rights of third parties; (d) respond to your requests for customer service; or (e) protect the rights, property or personal safety of YAPC its users and the public.</li>
 	</ul>
-	<h2 class="text-2xl font-bold mt-4">
-		4. CONTENT MADE AVAILABLE FOR INCLUSION ON THE YAPC SERVICE
-	</h2>
-	<p>
-		YAPC does not claim ownership of Content you submit or make available for inclusion via the YAPC
-		Service. However, with respect to Content you submit or make available for inclusion on the
-		publicly available YAPC Service, you irrevocably grant YAPC the perpetual, worldwide,
-		royalty-free and non-exclusive license, with the right to sublicense through multiple tiers of
-		sublicensees, to use, distribute, reproduce, modify, adapt, publicly perform and publicly
-		display such Content, in whole or in part, on the YAPC Service or other publications by YAPC in
-		any media whether now existing or which come into the existence into the future and to provide
-		that information under the license set forth on the initial page of the $url Web site. And for
-		Content you submit for private discussions, you grant YAPC the worldwide, royalty-free and
-		non-exclusive license to use, distribute, reproduce, modify, adapt, publicly perform and
-		publicly display such Content on the associated private discussion group.
-	</p>
+	<h2 class="text-2xl font-bold mt-4">4. CONTENT MADE AVAILABLE FOR INCLUSION ON THE YAPC SERVICE</h2>
+	<ul>
+	    <li>YAPC does not claim ownership of Content you submit or make available for inclusion via the YAPC Service. However, with respect to Content you submit or make available for inclusion on the publicly available YAPC Service, you irrevocably grant YAPC the perpetual, worldwide, royalty-free and non-exclusive license, with the right to sublicense through multiple tiers of sublicensees, to use, distribute, reproduce, modify, adapt, publicly perform and publicly display such Content, in whole or in part, on the YAPC Service or other publications by YAPC in any media whether now existing or which come into the existence into the future and to provide that information under the license set forth on the initial page of the YAPC Web site.</li>
+	    <li>For Content you submit for private discussions, you grant YAPC the worldwide, royalty-free and non-exclusive license to use, distribute, reproduce, modify, adapt, publicly perform and publicly display such Content on the associated private discussion group.</li>
+	</ul>
 
 	<h2 class="text-2xl font-bold mt-4">5. INDEMNITY</h2>
-	<p>
-		You agree to indemnify and hold YAPC and its subsidiaries, affiliates, officers, agents,
-		employees, partners and licensors harmless from any claim or demand, including but not limited
-		to reasonable attorneys' fees, made by any third party due to or arising out of Content you
-		submit, post, transmit or otherwise make available through the Service, your use of the YAPC
-		Service, your connection to the Service, your violation of the TOS, or your violation of any
-		rights of another.
-	</p>
+	<ul>
+	    <li>You agree to indemnify and hold YAPC and its subsidiaries, affiliates, officers, agents, employees, partners and licensors harmless from any claim or demand, including but not limited to reasonable attorneys' fees, made by any third party due to or arising out of Content you submit, post, transmit or otherwise make available through the Service, your use of the YAPC Service, your connection to the Service, your violation of the TOS, or your violation of any rights of another.</li>
+	</ul>
 
 	<h2 class="text-2xl font-bold mt-4">6. MODIFICATIONS TO YAPC SERVICE</h2>
-	<p>
-		You acknowledge that YAPC may establish general practices and limits concerning use of the YAPC
-		Service, including without limitation the maximum number of days that email messages, message
-		board postings or other uploaded Content will be retained by the YAPC Service.. You further
-		acknowledge that YAPC reserves the right to modify these general practices and limits from time
-		to time. YAPC reserves the right at any time and from time to time to modify or discontinue,
-		temporarily or permanently, the YAPC Service (or any part thereof) with or without notice. You
-		agree that YAPC shall not be liable to you or to any third party for any modification,
-		suspension or discontinuance of the YAPC Service.
-	</p>
+	<ul>
+	    <li>You acknowledge that YAPC may establish general practices and limits concerning use of the YAPC Service, including without limitation the maximum number of days that email messages, message board postings or other uploaded Content will be retained by the YAPC Service. You further acknowledge that YAPC reserves the right to modify these general practices and limits from time to time.</li>
+	    <li>YAPC reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, the YAPC Service (or any part thereof) with or without notice. You agree that YAPC shall not be liable to you or to any third party for any modification, suspension or discontinuance of the YAPC Service.</li>
+	</ul>
 
 	<h2 class="text-2xl font-bold mt-4">7. TERMINATION</h2>
-	<p>
-		You agree that YAPC may terminate your access to the YAPC Service for violations of the TOS
-		and/or requests by authorized law enforcement or other government agencies.
-	</p>
+	<ul>
+	    <li>You agree that YAPC may terminate your access to the YAPC Service for violations of the TOS and/or requests by authorized law enforcement or other government agencies.</li>
+	</ul>
 
 	<h2 class="text-2xl font-bold mt-4">8. LINKS</h2>
-	<p>
-		The YAPC Service may provide, or third parties may provide, links to other World Wide Web sites
-		or resources. Because YAPC has no control over such sites and resources, you acknowledge and
-		agree that YAPC is not responsible for the availability of such external sites or resources, and
-		does not endorse and is not responsible or liable for any Content, advertising, products or
-		other materials on or available from such sites or resources. You further acknowledge and agree
-		that YAPC shall not be responsible or liable, directly or indirectly, for any damage or loss
-		caused or alleged to be caused by or in connection with use of or reliance on any such Content,
-		goods or services available on or through any such site or resource.
-	</p>
+	<ul>
+	    <li>The YAPC Service may provide, or third parties may provide, links to other World Wide Web sites or resources. Because YAPC has no control over such sites and resources, you acknowledge and agree that YAPC is not responsible for the availability of such external sites or resources, and does not endorse and is not responsible or liable for any Content, advertising, products or other materials on or available from such sites or resources. You further acknowledge and agree that YAPC shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such Content, goods or services available on or through any such site or resource.</li>
+	</ul>
 
 	<h2 class="text-2xl font-bold mt-4">9. DISCLAIMER OF WARRANTIES</h2>
-	<p>
-		You expressly understand and agree that: Your use of the YAPC service is at your sole risk. YAPC
-		service is provided on an "as is" and "as available" basis. YAPC and its subsidiaries,
-		affiliates, officers, employees and licensors expressly disclaim all warranties of any kind,
-		whether express or implied, including, but not limited to the implied warranties of
-		merchantability, fitness for a particular purpose and non-infringement. YAPC and its
-		subsidiaries, affiliates, officers, employees and licensors make no warranty that (i) The YAPC
-		service will meet your requirements; (ii) The YAPC service will be uninterrupted, timely, secure
-		or error-free and (iii) The results that may be obtained from the use of the YAPC service will
-		be accurate or reliable. No advice or information, whether oral or written, obtained by you from
-		YAPC or through or from the service shall create any warranty not expressly stated in the TOS.
-	</p>
+	<ul>
+	    <li>You expressly understand and agree that: Your use of the YAPC service is at your sole risk. YAPC service is provided on an "as is" and "as available" basis. YAPC and its subsidiaries, affiliates, officers, employees and licensors expressly disclaim all warranties of any kind, whether express or implied, including, but not limited to the implied warranties of merchantability, fitness for a particular purpose and non-infringement. YAPC and its subsidiaries, affiliates, officers, employees and licensors make no warranty that (i) The YAPC service will meet your requirements; (ii) The YAPC service will be uninterrupted, timely, secure or error-free and (iii) The results that may be obtained from the use of the YAPC service will be accurate or reliable. No advice or information, whether oral or written, obtained by you from YAPC or through or from the service shall create any warranty not expressly stated in the TOS.</li>
+	</ul>
+
 	<h2 class="text-2xl font-bold mt-4">10. LIMITATION OF LIABILITY</h2>
-	<p>
-		You expressly understand and agree that YAPC and its subsidiaries, affiliates, officers,
-		employees, agents, partners and licensors shall not be liable to you for any direct, indirect,
-		incidental, special, consequential or exemplary damages, including, but not limited to, damages
-		for loss of profits, goodwill, use, data or other intangible losses (even if YAPC has been
-		advised of the possibility of such damages), resulting from the use or the inability to use the
-		YAPC service.
-	</p>
+	<ul>
+	    <li>You expressly understand and agree that YAPC and its subsidiaries, affiliates, officers, employees, agents, partners and licensors shall not be liable to you for any direct, indirect, incidental, special, consequential or exemplary damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if YAPC has been advised of the possibility of such damages), resulting from the use or the inability to use the YAPC service.</li>
+	</ul>
 
-	<h2 class="text-2xl font-bold mt-4">
-		11. NOTICE AND PROCEDURE FOR MAKING CLAIMS OF COPYRIGHT INFRINGEMENT
-	</h2>
-	<p>
-		YAPC respects the copyright of others, and we ask our users to do the same. YAPC may, in
-		appropriate circumstances and at its discretion, disable and/or terminate the access of users
-		who may be repeat infringers. If you believe that your work has been copied in a way that
-		constitutes copyright infringement, please provide YAPC's Copyright Agent the following
-		information: an electronic or physical signature of the person authorized to act on behalf of
-		the owner of the copyright; a description of the copyrighted work that you claim has been
-		infringed; a description of where the material that you claim is infringing is located on the
-		site; your address, telephone number, and email address; a statement by you that you have a good
-		faith belief that the disputed use is not authorized by the copyright owner, its agent, or the
-		law; a statement by you, made under penalty of perjury, that the above information in your
-		Notice is accurate and that you are the copyright owner or authorized to act on the copyright
-		owner's behalf. YAPC's Agent for Notice of claims of copyright infringement can be reached as
-		follows: admin@boofdev.eu
-	</p>
+	<h2 class="text-2xl font-bold mt-4">11. NOTICE AND PROCEDURE FOR MAKING CLAIMS OF COPYRIGHT INFRINGEMENT</h2>
+	<ul>
+	    <li>YAPC respects the copyright of others, and we ask our users to do the same. YAPC may, in appropriate circumstances and at its discretion, disable and/or terminate the access of users who may be repeat infringers. If you believe that your work has been copied in a way that constitutes copyright infringement, please provide YAPC's Copyright Agent the following information: an electronic or physical signature of the person authorized to act on behalf of the owner of the copyright; a description of the copyrighted work that you claim has been infringed; a description of where the material that you claim is infringing is located on the site; your address, telephone number, and email address; a statement by you that you have a good faith belief that the disputed use is not authorized by the copyright owner, its agent, or the law; a statement by you, made under penalty of perjury, that the above information in your Notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf. YAPC's Agent for Notice of claims of copyright infringement can be reached as follows: admin@boofdev.eu</li>
+	</ul>
 
-	This license text is under «Creative Commons - Attribution 3.0 Unported»
-	(https://creativecommons.org/licenses/by/3.0). It is based on the «Open Source Initiative Terms of
-	Service» (https://opensource.org/ToS).
+	<p>This license text is under «Creative Commons - Attribution 3.0 Unported» (https://creativecommons.org/licenses/by/3.0). It is based on the «Open Source Initiative Terms of Service» (https://opensource.org/ToS).</p>
 </div>
