0a1f499685829bb52fbfd3f7fa05751a573baf0a
Boofdev <hexahigh0@gmail.com>
2024-01-05 13:38:44 +0100

    update terms

--- frontend/src/routes/terms/+page.svelte
+++ frontend/src/routes/terms/+page.svelte
@@ -17,50 +17,28 @@
 	</p>
 
 	<h2 class="text-2xl font-bold mt-4">3. CONDUCT</h2>
-	<p>
-		You understand that all information, data, text, software, graphics or other materials
-		("Content"), whether publicly posted or privately transmitted, are the sole responsibility of
-		the person from whom such Content originated. This means that you, and not YAPC, are entirely
-		responsible for all Content that you upload, post, email, transmit or otherwise make available
-		via the YAPC Service. YAPC does not control the Content posted via the YAPC Service and, as
-		such, does not guarantee the accuracy, integrity or quality of such Content. Under no
-		circumstances will YAPC be liable in any way for any Content, including, but not limited to, any
-		errors or omissions in any Content, or any loss or damage of any kind incurred as a result of
-		the use of any Content posted, emailed, transmitted or otherwise made available via the YAPC
-		Service. You agree to not use the YAPC Service to: 1) upload, post, email, transmit or otherwise
-		make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortuous,
-		defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or racially,
-		ethnically or otherwise objectionable or harm minors; 2) impersonate any person or entity or
-		falsely state or otherwise misrepresent your affiliation with a person or entity; 3) forge
-		headers or otherwise manipulate identifiers in order to disguise the origin of any Content
-		transmitted through the YAPC Service; 4) upload, post, email, transmit or otherwise make
-		available any Content that you do not have a right to make available under any law or under
-		contractual or other relationships; 5) upload, post, email, transmit or otherwise make available
-		any Content that infringes any patent, trademark, trade secret, copyright or other proprietary
-		rights of any party; 6) upload, post, email, transmit or otherwise make available any
-		unsolicited or unauthorized advertising, promotional materials, "junk mail," "spam," or any
-		other form of solicitation; 7) upload, post, email, transmit or otherwise make available any
-		material that contains software viruses or any other computer code, files or programs designed
-		to interrupt, destroy or limit the functionality of any computer software or hardware or
-		telecommunications equipment; 8) disrupt the normal flow of dialogue, or otherwise act in a
-		manner that negatively affects other users' ability to engage in discussions or exchanges; 9)
-		interfere with or disrupt the YAPC Service or servers or networks connected to the YAPC Service,
-		or disobey any requirements, procedures, policies or regulations of networks connected to the
-		YAPC Service; 10) intentionally or unintentionally violate any applicable local, state, national
-		or international law, 11) "stalk" or otherwise harass another; and/or 12) collect or store
-		personal data about other users in connection with the prohibited conduct and activities set
-		forth in paragraphs above. You acknowledge that YAPC may or may not pre-screen Content, but that
-		YAPC and its designees shall have the right (but not the obligation) in their sole discretion to
-		pre-screen, refuse, move, or remove any Content that is available via the YAPC Service and which
-		violates the TOS. You agree that you must evaluate, and bear all risks associated with, the use
-		of any Content, including any reliance on the accuracy, completeness, or usefulness of such
-		Content. You acknowledge, consent and agree that YAPC may access, preserve and disclose your
-		account information and Content if required to do so by law or in a good faith belief that such
-		access preservation or disclosure is reasonably necessary to: (a) comply with legal process; (b)
-		enforce the TOS; (c) respond to claims that any Content violates the rights of third parties;
-		(d) respond to your requests for customer service; or (e) protect the rights, property or
-		personal safety of YAPC its users and the public.
-	</p>
+	<ul>
+	    <li>You understand that all information, data, text, software, graphics or other materials ("Content"), whether publicly posted or privately transmitted, are the sole responsibility of the person from whom such Content originated. This means that you, and not YAPC, are entirely responsible for all Content that you upload, post, email, transmit or otherwise make available via the YAPC Service.</li>
+	    <li>YAPC does not control the Content posted via the YAPC Service and, as such, does not guarantee the accuracy, integrity or quality of such Content. Under no circumstances will YAPC be liable in any way for any Content, including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via the YAPC Service.</li>
+	    <li>You agree to not use the YAPC Service to:</li>
+	    <ul>
+	        <li>Upload, post, email, transmit or otherwise make available any Content that is unlawful, harmful, threatening, abusive, harassing, tortuous, defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or racially, ethnically or otherwise objectionable or harm minors.</li>
+	        <li>Impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or entity.</li>
+	        <li>Forge headers or otherwise manipulate identifiers in order to disguise the origin of any Content transmitted through the YAPC Service.</li>
+	        <li>Upload, post, email, transmit or otherwise make available any Content that you do not have a right to make available under any law or under contractual or other relationships.</li>
+	        <li>Upload, post, email, transmit or otherwise make available any Content that infringes any patent, trademark, trade secret, copyright or other proprietary rights of any party.</li>
+	        <li>Upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional materials, "junk mail," "spam," or any other form of solicitation.</li>
+	        <li>Upload, post, email, transmit or otherwise make available any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment.</li>
+	        <li>Disrupt the normal flow of dialogue, or otherwise act in a manner that negatively affects other users' ability to engage in discussions or exchanges.</li>
+	        <li>Interfere with or disrupt the YAPC Service or servers or networks connected to the YAPC Service, or disobey any requirements, procedures, policies or regulations of networks connected to the YAPC Service.</li>
+	        <li>Intentionally or unintentionally violate any applicable local, state, national or international law.</li>
+	        <li>"Stalk" or otherwise harass another.</li>
+	        <li>Collect or store personal data about other users in connection with the prohibited conduct and activities set forth in paragraphs above.</li>
+	    </ul>
+	    <li>You acknowledge that YAPC may or may not pre-screen Content, but that YAPC and its designees shall have the right (but not the obligation) in their sole discretion to pre-screen, refuse, move, or remove any Content that is available via the YAPC Service and which violates the TOS.</li>
+	    <li>You agree that you must evaluate, and bear all risks associated with, the use of any Content, including any reliance on the accuracy, completeness, or usefulness of such Content.</li>
+	    <li>You acknowledge, consent and agree that YAPC may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to: (a) comply with legal process; (b) enforce the TOS; (c) respond to claims that any Content violates the rights of third parties; (d) respond to your requests for customer service; or (e) protect the rights, property or personal safety of YAPC its users and the public.</li>
+	</ul>
 	<h2 class="text-2xl font-bold mt-4">
 		4. CONTENT MADE AVAILABLE FOR INCLUSION ON THE YAPC SERVICE
 	</h2>
