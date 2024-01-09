fbe3dfa5423605f44bb1c49c8a877f947bacb9d7
Boofdev <admin@boofdev.eu>
2024-01-09 14:31:51 +0100

    add banner

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { writable } from 'svelte/store';
 	import { darkMode } from '$lib/dark.js';
+  import { DollarSolid } from 'flowbite-svelte-icons';
 	import '../app.css';
 
 	const currentClass = writable('mocha'); // Default class
@@ -12,12 +13,16 @@
 	}
 </script>
 
+<div class="bg-yellow-300 text-center p-2">
+  <a href="https://patreon.com/boofdev">Storage does not grow on trees. Consider becoming a Patron.</a>
+</div>
+
 <main
 	class="text-text bg-base min-h-screen"
 	class:mocha={$currentClass === 'mocha'}
 	class:latte={$currentClass === 'latte'}
 >
-<button on:click={toggleClass} class="absolute top-4 right-4 text-2xl w-20 z-50">
+<button on:click={toggleClass} class="absolute top-4 right-4 text-2xl w-20 z-10">
 	{#if $currentClass === 'mocha'}
 		🌑
 	{:else}
