99fd8e9145bb0faac827a9f4c428da4e637f9061
Boofdev <hexahigh0@gmail.com>
2024-01-24 14:09:33 +0100

    update report

--- backend/report.go
+++ backend/report.go
@@ -1,74 +1,103 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
-	"os/exec"
-	"runtime"
 	"strings"
 )
 
 type SystemReport struct {
-	CPUInfo string `json:"cpu_info"`
-	OS      string `json:"os"`
-	MEMInfo string `json:"mem_info"`
-	LSBLK   string `json:"lsblk"`
-	DF      string `json:"df"`
+	CPUInfo map[string]string `json:"cpu_info"`
+	OS      map[string]string `json:"os"`
+	MEMInfo map[string]string `json:"mem_info"`
 }
 
 func report(out_file string) {
 	fmt.Println("Starting system report")
 
 	// Get CPU Info
-	cpuInfo, err := os.ReadFile("/proc/cpuinfo")
+	cpuInfo, err := os.Open("/proc/cpuinfo")
 	if err != nil {
+		fmt.Println("Error opening /proc/cpuinfo:", err)
+		return
+	}
+	defer cpuInfo.Close()
+
+	scanner := bufio.NewScanner(cpuInfo)
+	cpuMap := make(map[string]string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			val := strings.TrimSpace(parts[1])
+			cpuMap[key] = val
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading /proc/cpuinfo:", err)
 		return
 	}
 
 	// Get OS Info
-	osInfo, err := os.ReadFile("/etc/os-release")
+	osInfo, err := os.Open("/etc/os-release")
 	if err != nil {
-		osInfo = []byte(runtime.GOOS)
-		if err != nil {
-			osInfo = []byte("failed")
+		fmt.Println("Error opening /etc/os-release:", err)
+		return
+	}
+	defer osInfo.Close()
+
+	scanner = bufio.NewScanner(osInfo)
+	osMap := make(map[string]string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := parts[0]
+			val := strings.Trim(parts[1], `"`)
+			osMap[key] = val
 		}
 	}
 
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading /etc/os-release:", err)
+		return
+	}
+
 	// Get mem info
-	memInfo, err := os.ReadFile("/proc/meminfo")
+	memInfo, err := os.Open("/proc/meminfo")
 	if err != nil {
+		fmt.Println("Error opening /proc/meminfo:", err)
+		return
+	}
+	defer cpuInfo.Close()
+
+	scanner = bufio.NewScanner(memInfo)
+	memMap := make(map[string]string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			val := strings.TrimSpace(parts[1])
+			memMap[key] = val
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading /proc/meminfo:", err)
 		return
 	}
 
-	// Get lsblk
-	lsblkCmd := exec.Command("lsblk")
-	lsblkOutput, err := lsblkCmd.Output()
-	if err != nil {
-		fmt.Println("Error running lsblk:", err)
-		return
-	}
-	lsblk := string(lsblkOutput)
-
-	// Get df
-	dfCmd := exec.Command("df", "-h")
-	dfOutput, err := dfCmd.Output()
-	if err != nil {
-		fmt.Println("Error running df:", err)
-		return
-	}
-	df := string(dfOutput)
-
 	// Create a new system report
 	report := &SystemReport{
-		CPUInfo: strings.TrimSpace(string(cpuInfo)),
-		OS:      strings.TrimSpace(string(osInfo)),
-		MEMInfo: strings.TrimSpace(string(memInfo)),
-		LSBLK:   strings.TrimSpace(lsblk),
-		DF:      strings.TrimSpace(df),
+		CPUInfo: cpuMap,
+		OS:      osMap,
+		MEMInfo: memMap,
 	}
 
 	// Marshal the report to JSON
